b9ddf3d8369399edd691ddf88a540a30fb6eee46
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-06-04 10:53:48 +0530

    corse policy added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const jsonServer=require("json-server");
 const server =jsonServer.create();
 const router=jsonServer.router("db.json");
-const middleware=jsonServer.defaults();
+// const middleware=jsonServer.defaults();
 const port=process.env.port || 3200;
+const middlewares = jsonServer.defaults({ noCors: true })
 
-server.use(middleware);
+server.use(middlewares);
 server.use(router);
 server.listen(port);

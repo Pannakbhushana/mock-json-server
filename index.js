0958fc9fd4fb18ad5b06e6fd24e810e1020b3dbd
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2023-06-04 11:03:37 +0530

    corse added

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const server =jsonServer.create();
 const router=jsonServer.router("db.json");
 const middleware=jsonServer.defaults();
 const port=process.env.port || 3200;
+const corse=require("cors");
 // const middlewares = jsonServer.defaults({ noCors: true })
 
+server.use(corse());
 server.use(middleware);
 server.use(router);
 server.listen(port);
